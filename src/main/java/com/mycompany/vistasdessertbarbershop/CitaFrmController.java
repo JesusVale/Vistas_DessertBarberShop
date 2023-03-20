8c5bb1663eb4ec9e4331e808993e6ebbeb001b00
Juan-Sotelo <juan.sotelo229391@potros.itson.edu.mx>
2023-03-20 14:34:53 -0700

    First Cummit
    Primer cummit

--- src/main/java/com/mycompany/vistasdessertbarbershop/CitaFrmController.java
+++ src/main/java/com/mycompany/vistasdessertbarbershop/CitaFrmController.java
@@ -2,9 +2,14 @@ package com.mycompany.vistasdessertbarbershop;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.fxml.FXML;
 
 public class CitaFrmController {
 
+    @FXML
+    private Button btnCasa;
+    
     @FXML
     private void switchToPrimary() throws IOException {
         App.setRoot("primary");
